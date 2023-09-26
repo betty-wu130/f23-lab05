e2117c2d7ef722eef71d9386255f8665d30e2a12
yuexin.wu <yuexin.wu@bytedance.com>
2023-09-26 16:45:49 -0400

    java: inheritance version, call add in father class and count number in a new var

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -1,5 +1,7 @@
 package edu.cmu.cs.cs214.rec04;
 
+import java.util.Comparator;
+
 /**
  * InheritanceSortedIntList -- a variant of a SortedIntList that keeps
  * count of the number of attempted element insertions (not to be confused
@@ -10,7 +12,40 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntLinkedList{
+    int totalAdded;
+    /**
+     * Constructor for InheritanceSortedIntList to initialize totalAdded.
+     */
+    InheritanceSortedIntList () {
+        totalAdded = 0;
+    }
+    
+    /**
+     * @param num the element to be inserted, while record in totalAdded if returned success.
+     * @return boolean whether the element is successfully inserted.
+     */
+    public boolean add(int num) {
+        if(super.add(num)) {
+            totalAdded += 1;
+            return true;
+        }
+        return false;
+    }
 
+    /**
+     * @param list the elements to be added, the number of successful addition is recorded
+     *              by total Added.
+     * @return boolean whether the total list is inserted successfully.
+     */
+    public boolean addAll(IntegerList list) {
+        return super.addAll(list);
+    }
+
+    /**
+     * @return The total number of numbers being added.
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
 }
